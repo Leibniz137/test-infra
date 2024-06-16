e033725ae3666d5815f7e2cb1776af9b72d9d019
Leibniz137 <nate@thenateway.io>
2024-06-16 16:30:59 -0700

    create service account key and save it to file

--- service_account.tf
+++ service_account.tf
@@ -4,3 +4,16 @@ resource "google_service_account" "terraform_sa" {
   account_id   = "terraform-sa"
   display_name = "Terraform Service Account"
 }
+
+resource "google_service_account_key" "terraform_sa_key" {
+  service_account_id = google_service_account.terraform_sa.name
+}
+
+resource "local_file" "service_account_key_file" {
+  content     = google_service_account_key.terraform_sa_key.private_key
+  filename = "${path.module}/terraform_sa_key.json"
+}
+
+output "service_account_email" {
+  value = google_service_account.terraform_sa.email
+}
