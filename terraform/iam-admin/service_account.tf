3d935b7afb90c2027ca1bb6c19b5fdf40cc89dfa
Leibniz137 <nate@thenateway.io>
2024-06-18 22:11:03 -0700

    create workload identity pool + provider

--- terraform/iam-admin/service_account.tf
+++ terraform/iam-admin/service_account.tf
@@ -5,15 +5,55 @@ resource "google_service_account" "terraform_sa" {
   display_name = "Terraform Service Account"
 }
 
-resource "google_service_account_key" "terraform_sa_key" {
-  service_account_id = google_service_account.terraform_sa.name
+variable "github_org" {
+  type    = string
+  default = "Leibniz137"
 }
 
-resource "local_file" "service_account_key_file" {
-  content     = google_service_account_key.terraform_sa_key.private_key
-  filename = "${path.module}/terraform_sa_key.json"
+variable "project_id" {
+  type    = string
+  default = "firewall-426619"
 }
 
-output "service_account_email" {
-  value = google_service_account.terraform_sa.email
+variable "workload_identity_pool_id" {
+  type    = string
+  default = "github"
+}
+
+# "In this setup, the Workload Identity Pool has direct IAM permissions on Google Cloud resources;
+# there are no intermediate service accounts or keys.
+# This is preferred since it directly authenticates GitHub Actions to Google Cloud without a proxy resource.
+# However, not all Google Cloud resources support principalSet identities.
+# Please see the documentation for your Google Cloud service for more information."
+#    - https://github.com/google-github-actions/auth/tree/v2/?tab=readme-ov-file#preferred-direct-workload-identity-federation
+
+
+# 1. Create a Workload Identity Pool
+resource "google_iam_workload_identity_pool" "github" {
+  workload_identity_pool_id = var.workload_identity_pool_id
+  display_name              = "GitHub Actions Pool"
+  description               = "Identity pool for automated test"
+  disabled                  = false
+  project                   = var.project_id
+}
+
+# 2. Create a Workload Identity Provider in that pool
+resource "google_iam_workload_identity_pool_provider" "sequencer_repo" {
+  # workload_identity_pool_id          = google_iam_workload_identity_pool.github.id
+  workload_identity_pool_id          = var.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-provider"
+
+  project      = var.project_id
+  display_name = "My GitHub repo Provider"
+  description  = "Identity pool provider for automated test"
+  attribute_condition = "assertion.repository_owner == '${var.github_org}'"
+  attribute_mapping = {
+    "google.subject" = "assertion.sub"
+    "attribute.actor" = "assertion.actor"
+    "attribute.repository" = "assertion.repository"
+    "attribute.repository_owner" = "assertion.repository_owner"
+  }
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
 }
