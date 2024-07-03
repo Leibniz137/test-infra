f5429499a40f60fa9ebb8fff7f9a15133c52384d
Leibniz137 <nate@thenateway.io>
2024-07-02 22:20:02 -0700

    install docker

--- main.tf
+++ main.tf
@@ -52,7 +52,28 @@ resource "google_compute_instance" "e2_micro" {
 
   metadata_startup_script = <<EOF
     #!/bin/bash
+
+    # installation directions here: https://docs.docker.com/engine/install/debian/
+
+    # Add Docker's official GPG key:
     sudo apt-get update
+    sudo apt-get install -y ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    # To install the latest version, run:
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+    # Verify that the installation is successful by running the hello-world image:
+    sudo docker run hello-world
   EOF
 
   network_interface {
