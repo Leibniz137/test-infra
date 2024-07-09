85cc08d0178eab86a5bf54b9dd9258a64b8170bf
Leibniz137 <nate@thenateway.io>
2024-07-08 22:32:04 -0700

    try enabling iam creds api

--- main.tf
+++ main.tf
@@ -37,6 +37,25 @@ provider "google" {
 #   location = "us-west2"
 # }
 
+# fixes "IAM Service Account Credentials API has not been used in project 812684586228 before or it is disabled."
+# NOTE: this is duplicated...
+resource "google_project_service" "iamcredentials" {
+  project = var.project_id
+
+  # service to enable
+  service = "iamcredentials.googleapis.com"
+
+  # If true, services that are enabled and which depend on this service should also be disabled when this service is destroyed.
+  # If false or unset, an error will be returned if any enabled services depend on this service when attempting to destroy it.
+  disable_dependent_services = false
+
+  # If true or unset, disable the service when the Terraform resource is destroyed.
+  # If false, the service will be left enabled when the Terraform resource is destroyed.
+  # Defaults to true. Most configurations should set this to false;
+  # it should generally only be true or unset in configurations that manage the google_project resource itself.
+  disable_on_destroy = false
+}
+
 data "google_service_account" "virtual_machine_sa" {
   # NOTE: must match what is defined in admin terraform
   account_id = "virtual-machine-sa"
