ec0a1b4ff6f1f711519b7ac7dffe71f34601cd39
Leibniz137 <nate@thenateway.io>
2024-07-08 22:47:40 -0700

    enable iam + pubsub apis

--- terraform/iam-admin/service_account.tf
+++ terraform/iam-admin/service_account.tf
@@ -193,6 +193,42 @@ resource "google_project_service" "compute" {
   disable_on_destroy = false
 }
 
+# fixes "Identity and Access Management (IAM) API has not been used in project 812684586228 before or it is disabled."
+resource "google_project_service" "iam" {
+  project = var.project_id
+
+  # service to enable
+  service = "iam.googleapis.com"
+
+  # If true, services that are enabled and which depend on this service should also be disabled when this service is destroyed.
+  # If false or unset, an error will be returned if any enabled services depend on this service when attempting to destroy it.
+  disable_dependent_services = false
+
+  # If true or unset, disable the service when the Terraform resource is destroyed.
+  # If false, the service will be left enabled when the Terraform resource is destroyed.
+  # Defaults to true. Most configurations should set this to false;
+  # it should generally only be true or unset in configurations that manage the google_project resource itself.
+  disable_on_destroy = false
+}
+
+# fixes "Cloud Pub/Sub API has not been used in project 812684586228 before or it is disabled."
+resource "google_project_service" "pubsub" {
+  project = var.project_id
+
+  # service to enable
+  service = "pubsub.googleapis.com"
+
+  # If true, services that are enabled and which depend on this service should also be disabled when this service is destroyed.
+  # If false or unset, an error will be returned if any enabled services depend on this service when attempting to destroy it.
+  disable_dependent_services = false
+
+  # If true or unset, disable the service when the Terraform resource is destroyed.
+  # If false, the service will be left enabled when the Terraform resource is destroyed.
+  # Defaults to true. Most configurations should set this to false;
+  # it should generally only be true or unset in configurations that manage the google_project resource itself.
+  disable_on_destroy = false
+}
+
 # fixes: "Permission 'iam.serviceAccounts.getAccessToken' denied on resource (or it may not exist)."
 resource "google_service_account_iam_binding" "allow_impersonation" {
   service_account_id = google_service_account.terraform_sa.name
